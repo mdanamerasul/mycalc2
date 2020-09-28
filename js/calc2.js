a55180ab9db7d5d72b21e92dc16825293719de88
mdanamerasul <49588321+mdanamerasul@users.noreply.github.com>
2020-09-28 13:33:26 +0600

    added new files

---
+++ js/calc2.js
@@ -0,0 +1,72 @@
+	function calc(){
+		document.form.calculator.value=
+		eval(document.form.calculator.value)
+	}
+	function c1(){
+		form.calculator.value += '1'
+	}
+
+	function c2(){
+		form.calculator.value += '2'
+	}
+	function c3(){
+		form.calculator.value += '3'
+	}
+	function c4(){
+		form.calculator.value += '4'
+	}
+
+	function c5(){
+		form.calculator.value += '5'
+	}
+
+	function c6(){
+		form.calculator.value += '6'
+	}
+	function c7(){
+		form.calculator.value += '7'
+	}
+	function c8(){
+		form.calculator.value += '8'
+	}
+	function c9(){
+		form.calculator.value += '9'
+	}
+	function c0(){
+		form.calculator.value += '0'
+	}
+	function cdot(){
+		form.calculator.value += '.'
+	}
+
+		function csum(){
+		form.calculator.value += '+'
+	}
+		function cmin(){
+		form.calculator.value += '-'
+	}
+		function cmul(){
+		form.calculator.value += '*'
+	}
+		function cdiv(){
+		form.calculator.value += '/'
+	}
+	
+	function csquare(){
+	document.form.calculator.value=
+		eval(Math.pow(document.form.calculator.value, 2))
+	}
+
+	
+	function croot(){
+	document.form.calculator.value=
+		eval(Math.sqrt(document.form.calculator.value))
+	}
+function con()
+
+{
+	document.form.calculator.value=
+	console.log(eval(document.form.calculator.value))
+}
+console.log(2+2)
+console.log(Math.sqrt(9))
